02dcd17d0c96bbe5d840fb542b2a14291e8235bc
Jaime Martinez Gamero <71594504+Jackesgamero@users.noreply.github.com>
2025-01-30 20:02:43 +0100

    Update AssertViolation.sol

--- contracts/AssertViolation.sol
+++ contracts/AssertViolation.sol
@@ -7,7 +7,14 @@ pragma solidity ^0.8.0;
 functioning code should never reach a failing assert statement */
 
 contract AssertViolation {
-    function causeAssertViolation() public pure {
-        assert(false);
+    uint256 public points;
+
+    function getPoints() external returns (uint256) {
+        return points;
+    }
+
+    function addPoints(uint256 amount) external {
+        points += amount;
+        assert(points >= amount);
     }
 }
