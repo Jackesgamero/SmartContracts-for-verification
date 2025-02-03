bcdb38a517906fdc1b32caa3e3900b56cd7cd043
Jaime Martinez Gamero <71594504+Jackesgamero@users.noreply.github.com>
2025-02-03 15:29:01 +0100

    commented no longer posible vulnerabilities

--- contracts/UninitializedStorage.sol
+++ contracts/UninitializedStorage.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//This vulnerability is no longer possible in version 0.8.0 of Solidity due to changes
-//in the compiler but will be studied in version 0.4.0
+////This vulnerability is no longer possible due to changes in the compiler
 
 //Author: Jaime Martinez Gamero
 
