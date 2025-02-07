370ce39deca91b7ede33967b44531a43066c222d
Jaime Martinez Gamero <71594504+Jackesgamero@users.noreply.github.com>
2025-02-07 12:35:30 +0100

    Update Timestamp.sol

--- contracts/Timestamp.sol
+++ contracts/Timestamp.sol
@@ -6,8 +6,8 @@ pragma solidity ^0.8.0;
 /* Contracts often need access to time values to perform certain types of functionality,
 malicious miners can alter the timestamp of their blocks, especially if they can gain advantages by doing so*/
 
-contract Timestamp {
-    function timestampDependence() public view returns (bool) {
-        return block.timestamp % 2 == 0;
+contract Lottery {
+    function lotteryWinner() public view returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(block.timestamp))) % 100000;
     }
 }
