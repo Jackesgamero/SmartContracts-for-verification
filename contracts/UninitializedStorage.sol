be7fabebaef6a2cc9f0405c1243264d2f361ffca
Jaime Martinez Gamero <71594504+Jackesgamero@users.noreply.github.com>
2025-02-15 21:30:06 +0100

    Update UninitializedStorage.sol

--- contracts/UninitializedStorage.sol
+++ contracts/UninitializedStorage.sol
@@ -1,16 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-////This vulnerability is no longer possible due to changes in the compiler
-
 //Author: Jaime Martinez Gamero
 
 //Uninitialized local storage variables can point to unexpected storage locations in the contract
 
 contract UninitializedStorage {
-    /*uint256[] private myArray;
-    function uninitializedPointer() external {
-        uint[] storage pointer;
-        pointer.push(1);
-    }*/
+    struct Data {
+        address user;
+        uint256 value;
+    }
+
+    Data[] public records;
+
+    function addRecord(uint256 _value) external {
+        Data memory newData;
+        newData.user = msg.sender;
+        newData.value = _value;
+        records.push(newData);
+    }
 }
