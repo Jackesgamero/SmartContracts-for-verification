fad451bd5312b68aadbf56f72263646d9417c72d
Jaime Martinez Gamero <71594504+Jackesgamero@users.noreply.github.com>
2025-02-21 18:02:09 +0100

    Add files via upload

--- contracts/BountyHunt.sol
+++ contracts/BountyHunt.sol
@@ -1,6 +1,6 @@
 /**
  *Submitted for verification at Etherscan.io on 2017-08-14
- */
+*/
 
 pragma solidity 0.5.2;
 
@@ -11,35 +11,32 @@ pragma solidity 0.5.2;
 //Modified lines 27,28
 
 contract BountyHunt {
-    mapping(address => uint) public bountyAmount;
-    uint public totalBountyAmount;
+  mapping(address => uint) public bountyAmount;
+  uint public totalBountyAmount;
 
-    modifier preventTheft() {
-        _;
-        if (address(this).balance < totalBountyAmount) revert("");
-    }
+  modifier preventTheft {
+    _;  
+    if (address(this).balance < totalBountyAmount) revert("");
+  }
 
-    function grantBounty(
-        address beneficiary,
-        uint amount
-    ) public payable preventTheft {
-        bountyAmount[beneficiary] += amount;
-        totalBountyAmount += amount;
-    }
+  function grantBounty(address beneficiary, uint amount) public payable preventTheft {
+    bountyAmount[beneficiary] += amount;
+    totalBountyAmount += amount;
+  }
 
-    function claimBounty() public preventTheft {
-        uint balance = bountyAmount[msg.sender];
-        (bool success, ) = msg.sender.call.value(balance)("");
-        if (success) {
-            totalBountyAmount -= balance;
-            bountyAmount[msg.sender] = 0;
-        }
-    }
+  function claimBounty() public preventTheft {
+    uint balance = bountyAmount[msg.sender];
+    (bool success,) = msg.sender.call.value(balance)("");
+    if (success) {
+      totalBountyAmount -= balance;
+      bountyAmount[msg.sender] = 0;
+    }   
+  }
 
-    function transferBounty(address to, uint value) public preventTheft {
-        if (bountyAmount[msg.sender] >= value) {
-            bountyAmount[to] += value;
-            bountyAmount[msg.sender] -= value;
-        }
-    }
+  function transferBounty(address to, uint value) public preventTheft {
+    if (bountyAmount[msg.sender] >= value) {
+      bountyAmount[to] += value;
+      bountyAmount[msg.sender] -= value;
+    }   
+  }
 }
