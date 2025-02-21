f2d87742bed12fa0bf617f23a52a45497dc0271f
Jaime Martinez Gamero <71594504+Jackesgamero@users.noreply.github.com>
2025-02-21 17:56:18 +0100

    Update BountyHunt.sol

--- contracts/BountyHunt.sol
+++ contracts/BountyHunt.sol
@@ -1,8 +1,8 @@
 /**
  *Submitted for verification at Etherscan.io on 2017-08-14
-*/
+ */
 
-pragma solidity ^0.4.4;
+pragma solidity 0.5.2;
 
 //Source: https://etherscan.io/address/0xb5766f61911f8b520b0e938aae100834aa3048c6#code
 
@@ -11,32 +11,35 @@ pragma solidity ^0.4.4;
 //Modified lines 27,28
 
 contract BountyHunt {
-  mapping(address => uint) public bountyAmount;
-  uint public totalBountyAmount;
+    mapping(address => uint) public bountyAmount;
+    uint public totalBountyAmount;
 
-  modifier preventTheft {
-    _;  
-    if (address(this).balance < totalBountyAmount) revert("");
-  }
+    modifier preventTheft() {
+        _;
+        if (address(this).balance < totalBountyAmount) revert("");
+    }
 
-  function grantBounty(address beneficiary, uint amount) public payable preventTheft {
-    bountyAmount[beneficiary] += amount;
-    totalBountyAmount += amount;
-  }
+    function grantBounty(
+        address beneficiary,
+        uint amount
+    ) public payable preventTheft {
+        bountyAmount[beneficiary] += amount;
+        totalBountyAmount += amount;
+    }
 
-  function claimBounty() public preventTheft {
-    uint balance = bountyAmount[msg.sender];
-    (bool success,) = msg.sender.call.value(balance)("");
-    if (success) {
-      totalBountyAmount -= balance;
-      bountyAmount[msg.sender] = 0;
-    }   
-  }
+    function claimBounty() public preventTheft {
+        uint balance = bountyAmount[msg.sender];
+        (bool success, ) = msg.sender.call.value(balance)("");
+        if (success) {
+            totalBountyAmount -= balance;
+            bountyAmount[msg.sender] = 0;
+        }
+    }
 
-  function transferBounty(address to, uint value) public preventTheft {
-    if (bountyAmount[msg.sender] >= value) {
-      bountyAmount[to] += value;
-      bountyAmount[msg.sender] -= value;
-    }   
-  }
+    function transferBounty(address to, uint value) public preventTheft {
+        if (bountyAmount[msg.sender] >= value) {
+            bountyAmount[to] += value;
+            bountyAmount[msg.sender] -= value;
+        }
+    }
 }
